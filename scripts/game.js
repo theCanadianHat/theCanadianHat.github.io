be124b4ea6e9ba7c9c83539886131525868c815d
Noah Herron <jayssbronze@gmail.com>
2015-09-16 20:31:33 -0400

    add player

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,8 @@
+//listen for load event to be trigger
 window.addEventListener("load",function() {
   var Q = window.Q = Quintus({development: true})
     .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
+    .include("ActionPlatformerPlayer")
     .setup({
       width: 320,   //to fit devices with a screne resolution of 1280 x 720
       height: 180,
@@ -9,13 +11,20 @@ window.addEventListener("load",function() {
 
     Q.setImageSmoothing(false);
 
-    //define scene
+    //define scene "level"
     Q.scene("level", function(stage){
       Q.stageTMX("myLevel.tmx", stage);
     });
 
     //load assets
-    Q.loadTMX("myLevel.tmx", function(){
+    //knows to look in "data" folder for tmx & json
+    //looks in "images" folder for png
+    Q.loadTMX("myLevel.tmx, sprites.json, sprites.png", function(){
+      //compile the sheets for the sprites.png which needs sprites.json
+      //using the json keys I can associate sprites with the right assests
+      Q.compileSheets("sprites.png","sprites.json");
+      //staging the screen
+      //what ever is on the screen is staged
       Q.stageScene("level");
     });
     
