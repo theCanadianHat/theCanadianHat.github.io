9c00ad5056773f732de846081cd736f26a406f0e
Noah Herron <jayssbronze@gmail.com>
2015-09-20 08:10:41 -0400

    added file enemy.js, added reference to index.html, added enemy component to game.js, added enemies to the level

---
+++ scripts/enemy.js
@@ -0,0 +1,9 @@
+Quintus.ActionPlatformerEnemy = function(Q){
+	Q.Sprite.extend("GroundEnemy",{
+		init: function(p){
+			this._super(p, {vx: -50, defaultDirection: "left"});
+			this.add("2d, aiBounce");
+		}
+	});
+
+};
--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@
 window.addEventListener("load",function() {
   var Q = window.Q = Quintus({development: true})
     .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
-    .include("ActionPlatformerPlayer")
+    .include("ActionPlatformerPlayer, ActionPlatformerEnemy")
     .setup({
       width: 320,   //to fit devices with a screne resolution of 1280 x 720
       height: 180,
