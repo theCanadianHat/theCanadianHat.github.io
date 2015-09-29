4451cf837d34dcb306e35ef67e26bfee3c557b8c
Noah Herron <jayssbronze@gmail.com>
2015-09-29 18:00:15 -0400

    bugs everywhere 4

--- scripts/player.js
+++ scripts/player.js
@@ -16,8 +16,8 @@ Quintus.ActionPlatformerPlayer = function(Q){
 			//eventually use pooling
 			var p = this.p;
 			Q.stage().insert(new Q.Bullet({
-				x: p.x,
-				y: p.y,
+				x: p.x + 15,
+				y: p.y + 15,
 				speed: 250
 			}));
 		}
